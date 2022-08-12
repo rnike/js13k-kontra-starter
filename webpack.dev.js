0318b4270a1ee196a0930ffb9fda55f491bb4090
Mike <yum650350@gmail.com>
2022-08-12 11:28:26 +0800

    build(deps): Bump version for all dependencies

--- webpack.dev.js
+++ webpack.dev.js
@@ -5,9 +5,10 @@ module.exports = merge(common, {
   mode: 'development',
   devtool: "eval-source-map",
   devServer: {
-		contentBase: './dist',
+		static: {
+			directory: "./dist"
+		},
 		hot: false,
-		inline: false,
 		host: "0.0.0.0"
   },
 });
