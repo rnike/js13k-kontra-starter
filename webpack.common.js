5d8deef2693f8c05b07f7f160a17b19617ec0133
Mike <yum650350@gmail.com>
2022-08-12 18:38:22 +0800

    build: use esbuild-loader to do three-shaking

--- webpack.common.js
+++ webpack.common.js
@@ -3,36 +3,40 @@ const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const TerserJSPlugin = require("terser-webpack-plugin");
 
 module.exports = {
-	optimization: {
-		minimizer: [new TerserJSPlugin({})]
-	},
-	plugins: [
-		new HtmlWebpackPlugin({ template: "src/index.html", inject: "body" }),
+  optimization: {
+    minimizer: [new TerserJSPlugin({})]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({ template: "src/index.html", inject: "body" }),
     new CleanWebpackPlugin(),
-	],
-	resolve: { extensions: [".ts", ".js"] },
-	module: {
-		rules: [
+  ],
+  resolve: { extensions: [".ts", ".js"] },
+  module: {
+    rules: [
+      {
+		test: /\.ts$/,
+		loader: "esbuild-loader",
+		exclude: /node_modules/,
+			options: {
+			loader: "ts",
+			target: "es2015",
+		},
+	},
+	{
+		test: /\.(png|jpg|gif)$/i,
+		use: [
 			{
-				test: /\.ts$/,
-				use: "ts-loader",
-				exclude: /node_modules/
-			},
-			{
-				test: /\.(png|jpg|gif)$/i,
-				use: [
-					{
-						loader: "url-loader",
-						options: {
-							limit: 8192
-						}
-					}
-				]
-			},
-			{
-				test: /\.glsl$/i,
-				use: "raw-loader"
+				loader: "url-loader",
+				options: {
+					limit: 8192
+				}
 			}
 		]
+	},
+	{
+		test: /\.glsl$/i,
+		use: "raw-loader"
 	}
+]
+}
 };
